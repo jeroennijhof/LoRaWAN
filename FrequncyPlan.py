123717d5c177774ed253a0e541a7603816259cb1
Philip Basford <pjb1u12@soton.ac.uk>
2018-01-18 15:27:23 +0000

    Missing file needed by Dragino class

---
+++ FrequncyPlan.py
@@ -0,0 +1,29 @@
+###############################################################################
+#Details of the frequncies available for LoRaWAN usage
+#Copyright (C) 2018 Philip Basford
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
+
+#https://www.thethingsnetwork.org/wiki/LoRaWAN/Frequencies/Frequency-Plans
+CH1 = 868.1
+CH2 = 868.3
+CH3 = 868.5
+CH4 = 867.1
+CH5 = 867.3
+CH6 = 867.5
+CH7 = 867.7
+CH8 = 867.9
+
+LORA_FREQS = (CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8)
